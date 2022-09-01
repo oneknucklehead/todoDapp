9961fa67532a86d69191cffa99b60bd28dec8c85
Zoheb Ahmed <zohebcool1542@gmail.com>
2022-09-01 15:23:47 +0530

    contracts code changed

--- contracts/contracts/tododapp.sol
+++ contracts/contracts/tododapp.sol
@@ -1,21 +1,31 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Tododapp {
-    constructor() {
-        addTask("Your task looks like this");
-    }
-
-    uint256 public totalTask = 0;
     struct Task {
-        uint256 id;
         bool completed;
         string task;
     }
 
-    mapping(uint256 => Task) public tasks;
+    mapping(address => Task[]) public Users;
 
-    function addTask(string memory _task) public {
-        totalTask++;
-        tasks[totalTask] = Task(totalTask, false, _task);
+    function addTask(string memory _task) external {
+        Users[msg.sender].push(Task({task: _task, completed: false}));
+    }
+
+    function getTask(uint256 _index) external view returns (Task memory) {
+        Task memory task = Users[msg.sender][_index];
+        return task;
+    }
+
+    function updateTask(uint256 _index, bool _status) external {
+        Users[msg.sender][_index].completed = _status;
+    }
+
+    function isDone(uint256 _index) external {
+        delete Users[msg.sender][_index];
+    }
+
+    function totalTasks() external view returns (uint256) {
+        return Users[msg.sender].length;
     }
 }
--- tododapp/src/App.js
+++ tododapp/src/App.js
@@ -1,20 +1,59 @@
 import { useEffect, useState } from 'react'
+import detectProvider from '@metamask/detect-provider'
 import Web3 from 'web3'
+import Task from './components/Task'
 
 function App() {
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null,
+  })
   const [account, setAccount] = useState()
+  useEffect(() => {
+    const loadProvider = async () => {
+      const provider = await detectProvider()
+      if (provider) {
+        setWeb3Api({
+          web3: new Web3(provider),
+          provider,
+        })
+      } else {
+        console.error('connect to your wallet')
+      }
+    }
+    loadProvider()
+  }, [])
 
   useEffect(() => {
-    async function load() {
-      const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545')
-      const accounts = await web3.eth.requestAccounts()
-
+    const getAccount = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts()
       setAccount(accounts[0])
     }
+    web3Api.web3 && getAccount()
+  }, [web3Api.web3])
 
-    load()
-  }, [])
-  return <div>Your account is: {account}</div>
+  console.log(web3Api.web3)
+  return (
+    <>
+      <div>
+        Your account is:{' '}
+        {account ? (
+          account
+        ) : (
+          <span>
+            <button
+              onClick={() =>
+                web3Api.provider.request({ method: 'eth_requestAccounts' })
+              }
+            >
+              Connect wallet
+            </button>
+          </span>
+        )}
+      </div>
+      <Task account={account} />
+    </>
+  )
 }
 
 export default App
